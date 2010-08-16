038f6c5fe34c248a4bd95be246cdf230587e6958
Zwooll <Zwooll@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-16 10:15:46 +0000

    new version
    git-svn-id: https://plugins.svn.wordpress.org/wiget-poster-s/tags/0.3@276411 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== Wiget Poster S ===
+Contributors: zwooll, deer
+Donate link: mailto:zwooee4@yahoo.com
+Tags: wigets, self, wiget, Wiget, post, s, widgets, self, widget, Widget
+Requires at least: 2.0.2
+Tested up to: 2.9.2
+Stable tag: 4.3
+
+This plugin is simple allow you creating own mini wiget.
+
+== Description ==
+
+This plugin is simple allow you creating own mini widget. And is a simple example of creating a widget in the plugin(usefull for developers).
+
+P.S. you can connect with my by emailing on: zwooee4@yahoo.com
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `widget_poster_s.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Way you create this plugin? =
+I have some free time and just have fun creating this.
+
+== Screenshots ==
+
+NO need screenshots
+
+== Changelog ==
+
+= 0.3 =
+* fix bug.
+
+= 0.2 =
+* Add more filds to edit widget.
+
+= 0.1 =
+* First version.
+
+== Upgrade Notice ==
+
+= 0.3 =
+fix separating text fild
+
+= 0.2 =
+Now you can place more information and ,for example, separate it with design...
+
+= 0.1 =
+Nothing.
---
+++ wiget_poster_s.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ *Plugin Name: Wiget Poster S
+ *Description: Plugin that allow post you widget.
+ *Version: 0.3
+ *Author: Deer
+ *Author URI: mailto:zwooee4@yahoo.com
+ */
+
+/**
+ * WIDGETS:
+ *
+ * widget class.
+ * This class handles everything that needs to be handled with the widget:
+ * the settings, form, display, and update.
+ */
+class WigetPosterS_Widget extends WP_Widget {
+	function WigetPosterS_Widget() {
+		parent::WP_Widget(false, $name = 'Wiget Poster S');
+	}
+
+	function widget($args, $instance) {
+		extract( $args );
+		echo '<div class = "widget_poster_s" id = "'.$this->id.'">';
+			if($instance['title'] != ''){
+				echo '<div class = "widget_poster_s-title">'.$instance['title'].'</div>';
+			}
+			if(($instance['text'] != '')||($instance['second_text'] != '')||($instance['third_text'] != '')){
+				echo '<div class = "widget_poster_s-textfilds">';
+				if ($instance['text'] != '') echo '<div class = "widget_poster_s-textfilds-text">'.$instance['text'].'</div>';
+				if ($instance['second_text'] != '') echo '<div class = "widget_poster_s-textfilds-second_text">'.$instance['second_text'].'</div>';
+				if ($instance['third_text'] != '') echo '<div class = "widget_poster_s-textfilds-third_text">'.$instance['third_text'].'</div>';
+				echo '</div>';
+			}
+		echo '</div>';
+	}
+
+	function update($new_instance, $old_instance) {
+		return $new_instance;
+	}
+
+	function form($instance) {
+		$title = esc_attr($instance['title']);
+		$text = esc_attr($instance['text']);
+		$second_text = $instance['second_text'];
+		$third_text = $instance['third_text'];
+
+		echo '
+		<p><label for="'.$this->get_field_id('title').'"> '._e('Title:').'</label>
+		<input class="widefat" id="'.$this->get_field_id('title').'"
+		name="'.$this->get_field_name('title').'" type="text"
+		value="'.$title.'" /></p>';
+		echo '
+		<p><label for="'.$this->get_field_id('text').'"> '._e('Main Text:').'</label>
+		<input class="widefat" id="'.$this->get_field_id('text').'"
+		name="'.$this->get_field_name('text').'" type="text"
+		value="'.$text.'" /></p>';
+		echo '
+		<p><label for="'.$this->get_field_id('second_text').'"> '._e('Second Text:').'</label>
+		<input class="widefat" id="'.$this->get_field_id('second_text').'"
+		name="'.$this->get_field_name('second_text').'" type="text"
+		value="'.$second_text.'" /></p>';
+		echo '
+		<p><label for="'.$this->get_field_id('third_text').'"> '._e('Third Text:').'</label>
+		<input class="widefat" id="'.$this->get_field_id('third_text').'"
+		name="'.$this->get_field_name('third_text').'" type="text"
+		value="'.$third_text.'" /></p>';
+	}
+
+}
+
+/**
+ * Register our widget.
+ * 'LinksManager_Widget' is the widget class used below.
+ */
+function WigetPosterS_load_widgets() {
+	register_widget( 'WigetPosterS_Widget' );
+}
+
+/**
+ * Add function to widgets_init that'll load our widget.
+ */
+add_action('widgets_init', create_function('', 'return register_widget("WigetPosterS_Widget");'));
+?>
